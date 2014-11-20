f60e6e54e0c72878e038dfaa4548345e674d2b1b
Mohammad Ali Khan <mohammad_ali93@live.com>
2014-11-20 00:44:19 +0000

    Add comments and remove hard-coded values

--- src/bt/Employee.java
+++ src/bt/Employee.java
@@ -2,56 +2,130 @@ package bt;
 
 import java.util.ArrayList;
 
+/**
+ * This is a class created for a representation for each Employee. The employee
+ * will have an id, a name and link to their manager. Employees also have a link
+ * to all the employees they are managers of. This functionality is not used at
+ * the moment but could be
+ * 
+ * @author Mohammad Ali
+ *
+ */
 public class Employee {
 	private int id;
 	private String name;
 	private Employee manager;
 	private ArrayList<Employee> managerOf;
-	public Employee(int employeeId, String name){
-		this(employeeId);
+
+	/**
+	 * Constructor for the class
+	 * 
+	 * @param id
+	 *            the employee id
+	 * @param name
+	 *            the employee name
+	 */
+	public Employee(int id, String name) {
+		this(id);
 		this.name = name.trim();
 	}
-	
-	public Employee(int id){
+
+	/**
+	 * Constructor for the class without the name
+	 * 
+	 * @param id
+	 *            the employee id
+	 */
+	public Employee(int id) {
 		this.id = id;
 		managerOf = new ArrayList<Employee>();
 	}
-	
-	
-	public String getName(){
+
+	/**
+	 * Method to retrieve employee name
+	 * 
+	 * @return the employee name
+	 */
+	public String getName() {
 		return name;
 	}
-	
-	public void setName(String name){
+
+	/**
+	 * Method to set employee name
+	 * 
+	 * @param name
+	 *            the employee name to be set
+	 */
+	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public int getId(){
+
+	/**
+	 * Method to retrieve employee id
+	 * 
+	 * @return the employee id
+	 */
+	public int getId() {
 		return id;
 	}
-	
-	public void setId(int employeeId){
+
+	/**
+	 * Method to set the employee id
+	 * 
+	 * @param employeeId
+	 *            the employee id to be set
+	 */
+	public void setId(int employeeId) {
 		this.id = employeeId;
-	}		
-	
-	public Employee getManager(){
+	}
+
+	/**
+	 * Method to retrieve manager of the employee
+	 * 
+	 * @return the employee manager
+	 */
+	public Employee getManager() {
 		return manager;
 	}
-	
-	public void setManager(Employee manager){
+
+	/**
+	 * Method to set manager of the employee
+	 * 
+	 * @param manager
+	 *            the manager to be set
+	 */
+	public void setManager(Employee manager) {
 		this.manager = manager;
 	}
-	
-	public void addEmployee(Employee employee){
+
+	/**
+	 * Add an employee to the list of employees this one manages
+	 * 
+	 * @param employee
+	 *            the employee to be added
+	 */
+	public void addEmployee(Employee employee) {
 		this.managerOf.add(employee);
 	}
-	
-	public ArrayList<Employee> getEmployees(){
+
+	/**
+	 * Retrieve the list of employees managed by this employee
+	 * 
+	 * @return the list of employees managed by this employee
+	 */
+	public ArrayList<Employee> getEmployees() {
 		return managerOf;
 	}
-	
-	public boolean equals(Employee employee){
+
+	/**
+	 * Custom method to compare two employees
+	 * 
+	 * @param employee
+	 *            the employee to be compared
+	 * @return confirmation or declination of equality
+	 */
+	public boolean equals(Employee employee) {
 		return this.id == employee.getId();
 	}
-	
+
 }
--- src/bt/OrganisationalChart.java
+++ src/bt/OrganisationalChart.java
@@ -8,139 +8,249 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.regex.Pattern;
 
+/**
+ * This class creates an organisational chart and find the shortest path between
+ * the two specified employees. This could also be used to build a tree and used
+ * for other purposes
+ * 
+ * @author Mohammad Ali
+ *
+ */
 public class OrganisationalChart {
+
 	private static HashMap<String, Integer> namesMap;
 	private static HashMap<Integer, Employee> employeeMap;
 	private static Employee ceo;
+
+	/**
+	 * Main method which takes in the parameters and finds the shortest path
+	 * 
+	 * @param args
+	 *            parameters which will be taken in (filename, employee 1 and
+	 *            employee 2)
+	 */
 	public static void main(String[] args) {
-		String fileName = "files/in/superheroes.txt";
-		String name1 = "Batman";
-		String name2 = "    batman";
-		if(args.length > 0){
-			fileName = args[0];
-			name1 = args[1];
-			name2 = args[2];
+		// usage is incorrect
+		if (args.length > 0) {
+			String fileName = args[0];
+			String name1 = args[1];
+			String name2 = args[2];
+			try {
+				// create our chart
+				buildChart(fileName);
+			} catch (FileNotFoundException e) {
+				System.err
+						.println("Error! There was something wrong with the file specified");
+			} catch (IOException e) {
+				System.err
+						.println("Error! Something went wrong while reading the file");
+				e.printStackTrace();
+			}
+			Employee employee1 = employeeMap.get(namesMap
+					.get(constructNormalisedName(name1.split("\"")[0])));
+			Employee employee2 = employeeMap.get(namesMap
+					.get(constructNormalisedName(name2.split("\"")[0])));
+			findWay(employee1, employee2);
+		} else {
+			System.err
+					.println("Error! Usage is OrganisationalChart (filename) (employee1) (employee2)");
 		}
-		try {
-			buildMaps(fileName);
-		} catch (FileNotFoundException e) {
-			System.err.println("Error! There was something wrong with the file specified");
-		} catch (IOException e) {
-			System.err.println("Error! Something went wrong while reading the file");
-			e.printStackTrace();
-		}
-		Employee employee1 = employeeMap.get(namesMap.get(Util.constructNormalisedName(name1)));
-		Employee employee2 = employeeMap.get(namesMap.get(Util.constructNormalisedName(name2)));
-		findWay(employee1,employee2);
+
 	}
-	
-	public static void buildMaps(String fileName) throws IOException{
+
+	/**
+	 * Method to create our organisational chart
+	 * 
+	 * @param fileName
+	 *            the text file containing all the employee records
+	 * @throws IOException
+	 *             possible problems in opening and reading through the file
+	 */
+	public static void buildChart(String fileName) throws IOException {
 		namesMap = new HashMap<String, Integer>();
 		employeeMap = new HashMap<Integer, Employee>();
-		BufferedReader buff  = new BufferedReader(new FileReader(fileName));
+		BufferedReader buff = new BufferedReader(new FileReader(fileName));
 		String line = buff.readLine();
-		line = buff.readLine(); //the headings
-		while(line != null){
+		line = buff.readLine(); // the headings
+		while (line != null) {
 			parseEmployee(line.trim());
 			line = buff.readLine();
 		}
 		buff.close();
 	}
-	
-	public static void parseEmployee(String line){
+
+	/**
+	 * Method to create an Employee class for each line in the text file and add
+	 * references to the HashMap
+	 * 
+	 * @param line
+	 *            an employee record
+	 */
+	public static void parseEmployee(String line) {
 		String[] bits = line.split(Pattern.quote("|"));
 		int start = 0;
-		if(bits[start].length() == 0){
+		if (bits[start].length() == 0) {
 			start = 1;
 		}
 		int employeeId = Integer.parseInt(bits[start].trim());
-		String name = bits[start+1];
+		String name = bits[start + 1];
 		Employee employee = employeeMap.get(employeeId);
-		if(employee == null){
-			//employee does not exist so create a new one
+		if (employee == null) {
+			// employee does not exist so create a new one
 			employee = new Employee(employeeId, name);
-		}else{
-			//employee was already created, so we set the unset values
+		} else {
+			// employee was already created, so we set the unset values
 			employee.setName(name);
 		}
-		String managerString = bits[start+2].trim();
-		if(managerString.length() > 0){
+		String managerString = bits[start + 2].trim();
+		if (managerString.length() > 0) {
 			int managerId = Integer.parseInt(managerString);
 			Employee manager = employeeMap.get(managerId);
-			if(manager == null){ 
-			//manager does not exist so we create a new one
-			manager = new Employee(managerId);				
+			if (manager == null) {
+				// manager does not exist so we create a new one
+				manager = new Employee(managerId);
 			}
 			employee.setManager(manager);
 			manager.addEmployee(employee);
 			employeeMap.put(managerId, manager);
-		}else{
-			//this is the root of our tree
+		} else {
+			// this is the root of our tree
 			ceo = employee;
 		}
-		namesMap.put(Util.constructNormalisedName(employee.getName()), employee.getId());
+		namesMap.put(constructNormalisedName(employee.getName()),
+				employee.getId());
 		employeeMap.put(employee.getId(), employee);
 	}
-	
-	public static void findWay(Employee to,Employee from){
-		int ancestor = findLowestCommonAncestor(to,from);
+
+	/**
+	 * Method to find the shortest path between the employees and printing it
+	 * 
+	 * @param to
+	 *            the employee from whom we want to find the path
+	 * @param from
+	 *            the employee to whom we want to find the path
+	 */
+	public static void findWay(Employee to, Employee from) {
+		int ancestor = findLowestCommonAncestor(to, from);
 		printPaths(to, from, employeeMap.get(ancestor));
 	}
-	
-	public static int findLowestCommonAncestor(Employee to, Employee from){
+
+	/**
+	 * Method to find the lowest common ancestor between the two employees
+	 * 
+	 * @param to
+	 *            the employee from whom we want to find the path
+	 * @param from
+	 *            the employee to whom we want to find the path
+	 * @return the lowest common ancestor between the two employees
+	 */
+	public static int findLowestCommonAncestor(Employee to, Employee from) {
 		ArrayList<Integer> paths = getPathToRoot(to);
-		while(!from.equals(ceo)){
+		while (!from.equals(ceo)) {
 			int id = from.getId();
-			if(paths.contains(id)){
+			if (paths.contains(id)) {
 				return id;
-			}else{
+			} else {
 				from = from.getManager();
 			}
 		}
 		return ceo.getId();
 	}
-	
-	public static ArrayList<Integer> getPathToRoot(Employee employee){
+
+	/**
+	 * Method to find the path from an employee (node) to the ceo (root)
+	 * 
+	 * @param employee
+	 *            the employee from which to find the path
+	 * @return the list of the employee ids present in this path
+	 */
+	public static ArrayList<Integer> getPathToRoot(Employee employee) {
 		ArrayList<Integer> paths = new ArrayList<Integer>();
 		paths.add(employee.getId());
-		while(!employee.equals(ceo)){
+		while (!employee.equals(ceo)) {
 			Employee manager = employee.getManager();
 			paths.add(manager.getId());
 			employee = manager;
-		}		
+		}
 		return paths;
 	}
-	
-	public static void printPaths(Employee to, Employee from, Employee ancestor){
-		if(to.equals(from)){
+
+	/**
+	 * Method to print the shortest path between the two specified employees
+	 * 
+	 * @param to
+	 *            the employee from whom we want to find the path
+	 * @param from
+	 *            the employee to whom we want to find the path
+	 * @param ancestor
+	 *            the lowest common ancestor between the two employees
+	 */
+	public static void printPaths(Employee to, Employee from, Employee ancestor) {
+		if (to.equals(from)) {
 			System.out.println(to.getName() + " (" + to.getId() + ")");
-		}else if(from.equals(ancestor)){
-			System.out.println(printTo(to, ancestor) + ancestor.getName() + " (" + ancestor.getId() + ")");
-		}else{
-		System.out.println(printTo(to, ancestor) + ancestor.getName()+ " (" + ancestor.getId() + ")" + printFrom(from, ancestor));
+		} else if (from.equals(ancestor)) {
+			System.out.println(getPathTo(to, ancestor) + ancestor.getName()
+					+ " (" + ancestor.getId() + ")");
+		} else {
+			System.out.println(getPathTo(to, ancestor) + ancestor.getName()
+					+ " (" + ancestor.getId() + ")"
+					+ getPathFrom(from, ancestor));
 		}
 	}
-		
-	
-	public static String printTo(Employee to, Employee from){
+
+	/**
+	 * Method to write path from an employee to another. This will be from the
+	 * employee to the ancestor
+	 * 
+	 * @param to
+	 *            the employee from whom we want to write the path
+	 * @param from
+	 *            the employee to whom we want to write the path
+	 * @return the string containing the path
+	 */
+	public static String getPathTo(Employee to, Employee from) {
 		String result = "";
 		String space = " -> ";
-		while(!to.equals(from)){
-			result+= to.getName() + " (" + to.getId() + ")" + space;
+		while (!to.equals(from)) {
+			result += to.getName() + " (" + to.getId() + ")" + space;
 			to = to.getManager();
 		}
 		return result;
 	}
-	
-	public static String printFrom(Employee to, Employee from){
+
+	/**
+	 * Method to write path to an employee to another. This will be from the
+	 * ancestor to the employee
+	 * 
+	 * @param to
+	 *            the employee from whom we want to write the path
+	 * @param from
+	 *            the employee to whom we want to write the path
+	 * @return the string containing the path
+	 */
+	public static String getPathFrom(Employee to, Employee from) {
 		String result = "";
 		String space = " <- ";
-		while(!to.equals(from)){
+		while (!to.equals(from)) {
 			result = space + to.getName() + " (" + to.getId() + ")" + result;
 			to = to.getManager();
 		}
 		return result;
 	}
-	
-	
+
+	/**
+	 * Utility method to construct normalised string from given name
+	 * 
+	 * @param originalName
+	 *            the given name which may have unecessary whitespaces
+	 * @return the normalised name
+	 */
+	public static String constructNormalisedName(String originalName) {
+		StringBuilder sb = new StringBuilder();
+		String[] bits = originalName.toLowerCase().trim().split(" ");
+		for (String bit : bits) {
+			sb.append(bit);
+		}
+		return sb.toString();
+	}
 }
